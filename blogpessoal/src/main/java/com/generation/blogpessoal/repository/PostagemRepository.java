0c350302a4206e7f151fd31b5cee4193c32a7068
renatonunes74 <renato.s.nunes74@gmail.com>
2023-04-14 16:21:59 -0300

    adicionando verbos HTTP

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,17 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
+import java.util.List;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-
+	public Optional<Postagem> findByTitulo(String titulo);
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
