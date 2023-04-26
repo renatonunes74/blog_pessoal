73b873191dbda9a08dcaadd9e4b1c4a004bc481a
renatonunes74 <renatonunes74@gmail.com>
2023-04-26 09:43:09 -0300

    sprint security

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -60,7 +60,7 @@ public class TemaController {
     public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
         return temaRepository.findById(tema.getId())
             .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
-            .body(temaRepository.save(tema)))
+				.body(temaRepository.save(tema)))
             .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
     
